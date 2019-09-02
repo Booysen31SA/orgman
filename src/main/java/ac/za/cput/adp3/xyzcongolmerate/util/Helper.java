3796c26091af31390e5bb0605cce78cc72262aa8
Matthew Booysen <MatthewBoo@awards.co.za>
2019-09-02 15:01:26 +0200

    commenting Helper class

--- src/main/java/ac/za/cput/adp3/xyzcongolmerate/util/Helper.java
+++ src/main/java/ac/za/cput/adp3/xyzcongolmerate/util/Helper.java
@@ -14,15 +14,15 @@ public class Helper {
 
     public static String getSuffixFromClassName(Class<?> aClass) {
         String className = getClassName(aClass); // gets the name of the class
-        int length = className.length(); // gets the length of the class name
-        String code = "";
+        int lengthOfClassName = className.length(); // gets the length of the class name
+        String upperCaseLetters = "";
 
-        for(int i = 0; i < length; i++){   //loops through each letter in the class name
+        for(int i = 0; i < lengthOfClassName; i++){   //loops through each letter in the class name
             if(Character.isUpperCase(className.charAt(i))){  // checks if the letter is uppercase
               char upperCase = className.charAt(i);   // copies the uppercase letter to a variable
-              code = code + upperCase + ""; // adds each uppercase to the string
+                upperCaseLetters = upperCaseLetters + upperCase + ""; // adds each uppercase to the string
             }
         }
-        return code; // returns all the capital letters
+        return upperCaseLetters; // returns all the capital letters
     }
 }
